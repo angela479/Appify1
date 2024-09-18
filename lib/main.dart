376d140fa0febe9fdaecb1e884ee1aadd043969e
Your Name <angelaa.bera@gmail.com>
2024-09-18 09:57:03 +0000

    done

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,87 @@ void main() {
   runApp(const MyApp());
 }
 
-
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return ;
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+            body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                'https://images.pexels.com/photos/1054218/pexels-photo-1054218.jpeg?cs=srgb&dl=pexels-stywo-1054218.jpg&fm=jpg',
+              ),
+              fit: BoxFit.cover, // This makes the image cover the entire screen
+            ),
+          ),
+          child: Container(
+            padding: EdgeInsets.all(20),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "India",
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+                Row(children: [
+                  Text(
+                    "32",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 120,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: SizedBox(
+                        width: 150,
+                        child: Image.network(
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFIxN7xdDuKsOhsDaru3eyYAJ3xwposjkSvQ&s")),
+                  ),
+                ]),
+                SizedBox(
+                  height: 280,
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.5),
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '64%\nHumidity',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      Text(
+                        '4 KM\nVisibility',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      Text(
+                        'Low 0%\nVisibility',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )),
+      ),
+    );
   }
 }
